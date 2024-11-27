ccc9ff1dc3c4ddfd072d84f9b136cd6e71ae027f
mohdsahilkhan9799 <107064173+mohdsahilkhan9799@users.noreply.github.com>
2024-11-27 11:09:38 +0530

    error solve

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/', // Adjust if your project is served from a subdirectory
-  plugins: [react()],
-});
