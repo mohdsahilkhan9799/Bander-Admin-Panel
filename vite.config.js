0cd2dfd483549a678ea6102a4f385bf20af78838
mohdsahilkhan9799 <107064173+mohdsahilkhan9799@users.noreply.github.com>
2024-12-03 12:52:19 +0530

    repo

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'dist'
-  }
+  base: '/<your-repo-name>/',  // Replace <your-repo-name> with your GitHub repository name
 });
