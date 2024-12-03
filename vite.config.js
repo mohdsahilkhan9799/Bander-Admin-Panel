72b31a37842339baf1186ab7e456118a1f2a34a3
mohdsahilkhan9799 <107064173+mohdsahilkhan9799@users.noreply.github.com>
2024-12-03 12:40:47 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure the output directory matches Vercel's expectations
-  },
   server: {
-    host: true, // Enables access via network if needed
+    port: 3000,
   },
+  build: {
+    outDir: 'dist'
+  }
 });
