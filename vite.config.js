0a2a9ad52d1c5d72979167810595b537d3d9d556
mohdsahilkhan9799 <107064173+mohdsahilkhan9799@users.noreply.github.com>
2024-12-03 14:45:50 +0530

    worked url

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-base: '/Bander-Admin-Panel', // This should match the name of your GitHub repository
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist'
+  }
 });
